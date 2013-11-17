6e3319d3ad3ffcef71a2f24128d074b99e0de588
Michael Kolodny <michaelckolodny@gmail.com>
2013-11-16 21:17:01 -0500

    add bdist_wininst to setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
     from distutils.core import setup
 
 if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+    os.system('python setup.py sdist bdist_wininst upload')
     sys.exit()
 
 with open('README.rst') as f:
