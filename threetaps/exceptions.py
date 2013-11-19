9edc11254e0fd9d88e93385c9000329e316096ac
Michael Kolodny <michaelckolodny@gmail.com>
2013-11-19 02:35:40 -0500

    remove shell files

--- threetaps/exceptions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-threetaps.exceptions
-~~~~~~~~~~~~~~~~~~~
-
-This module contains the set of threetaps' exceptions.
-
-"""
--- threetaps/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2013 Michael Kolodny
-
-"""
-threetaps.models
-~~~~~~~~~~~~~~~
-
-This module contains the primary objects that power threetaps.
-"""
