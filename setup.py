257f09108c2f7c646a92d51b8736df3da628df13
mkolodny <michaelckolodny@gmail.com>
2014-12-08 16:19:17 -0500

    chore(setup.py): specify where to upload to when publishing

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
     from distutils.core import setup
 
 if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wininst upload')
+    os.system('python setup.py sdist bdist_wininst upload -r pypi')
     sys.exit()
 
 with open('README.rst') as f:
